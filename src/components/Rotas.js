4658b8d9396ae28028b016a2396d621ac26aa76c
joel bispo <joelbispo04@hotmail.com>
2023-10-03 15:22:49 -0300

    feito a requisição de dados do usuario atraves do login

--- src/components/Rotas.js
+++ src/components/Rotas.js
@@ -14,7 +14,6 @@ const Rotas = () => {
                            element={
                             auth ? <Home/> : <Navigate to='/login'/>
                            }/>
-
                 </Routes>
             </Router>
         )
