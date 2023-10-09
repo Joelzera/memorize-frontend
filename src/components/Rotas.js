36f4033391070e88fa2f7e6c945a5f6e1e8e4093
joel bispo <joelbispo04@hotmail.com>
2023-10-09 11:13:54 -0300

    redirecionamento de rota login autenticado

--- src/components/Rotas.js
+++ src/components/Rotas.js
@@ -1,19 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { isAuthenticated } from './Auth'
 import Home from './Home';
 import Login from './Login';
 
-const auth = isAuthenticated()
+
 
 const Rotas = () => {
         return(
             <Router>
                 <Routes>
                     <Route exact path='/login' element={<Login />}/>
-                    <Route exact path='/'
-                           element={
-                            auth ? <Home/> : <Navigate to='/login'/>
-                           }/>
+                    <Route exact path='/' element={<Home/>}/>
                 </Routes>
             </Router>
         )
